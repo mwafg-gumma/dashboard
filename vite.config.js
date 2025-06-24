9af4260336f6bf5630b0c57a7a924554ccf893a4
mwafg gumma <mwafggommaabdula@gmail.com>
2025-06-24 17:54:21 +0200

    Fix: Update base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Dashboard-React-Dev/",
   server: {
     port:3000,
-  }
+  },
+  base:"/statistics-dev/"
 })
