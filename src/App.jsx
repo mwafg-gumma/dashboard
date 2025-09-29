451ecd1e273f0fc880eef31e6300c66a8682afdb
mwafg gumma <mwafggommaabdula@gmail.com>
2025-09-29 15:25:31 +0530

    Edit : Add basename in App.jsx Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,4 @@
 
-// import {  Route,
-//   createBrowserRouter,
-//   createRoutesFromElements,
-//   RouterProvider
-// } from "react-router-dom";
-// import TablePage from "./components/Links/TablePage";
-// import HomePage from "./components/Home/HomePage";
-// import BillingPage from "./components/Links/BillingPage";
-// import ProfilePage from "./components/Links/ProfilePage";
-// import Form from "./components/Form";
-// import SingUp from "./components/SingUp";
-
-// const router = createBrowserRouter (
-//   createRoutesFromElements (
-//   <Route path="/statistics-panel" element={<HomePage />} />
-//   <Route path="/table" element={<TablePage />} />
-//   <Route path="/bill" element={<BillingPage />} />
-//   <Route path="/profile" element={<ProfilePage />} />
-//   <Route path="/SigIn" element={<Form />} />
-//   <Route path="/SigUp" element={<SingUp />} />
-//   </Route>
-//   ),
-//   {
-//   }
-// )
-
-// const App = () => {
-//   return <RouterProvider router={router} />
-// }
-
-// export default App;
-
 import {
   Route,
   createBrowserRouter,
@@ -49,10 +17,6 @@ import DetailsDashoardPage from "./components/Links/DetailsDashoardPage";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-    {/* <Route index element={<h1>Welcome</h1>} >
-    
-    </Route> */}
-
       <Route path="/" element={<HomePage />} />
       <Route path="/table" element={<TablePage />} />
       <Route path="/bill" element={<BillingPage />} />
@@ -61,7 +25,10 @@ const router = createBrowserRouter(
       <Route path="/sigIn" element={<Form />} />
       <Route path="/sigUp" element={<SingUp />} />
     </>
-  )
+  ),
+  {
+    basename: "/dashboard", 
+  }
 );
 
 const App = () => {
